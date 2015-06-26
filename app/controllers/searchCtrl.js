1b5c15c43ef92f78768c0a06c5585d577be571cb
Jonas Meuleman <jonas.meuleman@gmail.com>
2015-06-26 16:29:44 +0200

    Initial commit

---
+++ app/controllers/searchCtrl.js
@@ -0,0 +1,32 @@
+
+function searchCtrl($scope, $http, apiUrl, $log, $location) {
+    $scope.message="Hello world!";
+
+    $scope.searchMovie=function(title) {
+        $log.debug(title);
+
+        var url = apiUrl + 'Movies/search?title=' + title;
+
+        $http.get(url).success(function(data) {
+            //$log.debug(data)
+            $scope.results = data;
+        });
+
+
+        $log.debug('run');
+    }
+
+    $scope.selectMovie = function(id) {
+        $scope.selectedId=id;
+    }
+
+    $scope.addMovie = function(id) {
+        var url = apiUrl + 'movies/' + id;
+        $http.post(url).success(function() {
+            $location.url('/collection');
+        });
+    }
+}
+
+angular.module('movieApp.controllers')
+    .controller('searchCtrl', searchCtrl);
